2958da06d5e9e09c60be696f4968f95963ead2ce
Jenny Weijland <jennyweij@outlook.com>
2023-12-05 10:25:57 +0100

    added routing for all 5 pages

---
+++ src/Pages/Bookmarked.tsx
@@ -0,0 +1,5 @@
+function Bookmarked() {
+  return <div>Bookmarked page</div>
+}
+
+export default Bookmarked
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { RouterProvider, createRoutesFromElements } from 'react-router'
+import { Route, createBrowserRouter } from 'react-router-dom'
 import App from './App.tsx'
+import Bookmarked from './Pages/Bookmarked.tsx'
+import Categories from './Pages/Categories.tsx'
+import CategorySpecific from './Pages/CategorySpecific.tsx'
+import FilmView from './Pages/FilmView.tsx'
+import HomePage from './Pages/HomePage.tsx'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<App />}>
+      <Route index element={<HomePage />} />
+      <Route path='filmview' element={<FilmView />} />
+      <Route path='bookmarked' element={<Bookmarked />} />
+      <Route path='categories' element={<Categories />} />
+      <Route path='categories/genre' element={<CategorySpecific />} />
+    </Route>
+  )
+)
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <RouterProvider router={router} />
+  </React.StrictMode>
 )
