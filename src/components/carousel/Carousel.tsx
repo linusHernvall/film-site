a1eeeb883891609db23f237f212b815ad37ed130
Linus Hernvall <linus.hernvall@gmail.com>
2023-12-05 18:02:04 +0100

    renders cards depending on screen width

--- src/components/carousel/Carousel.tsx
+++ src/components/carousel/Carousel.tsx
@@ -1,5 +1,5 @@
 import { Box as MuiBox } from '@mui/material'
-import { useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { Movie } from '../../interface'
 import { Card, NextArrow, PrevArrow } from './style'
 
@@ -9,6 +9,23 @@ interface ICarousel {
 
 export const Carousel = ({ movies }: ICarousel) => {
   const [current, setCurrent] = useState(0)
+  const [visibleMoviesCount, setVisibleMoviesCount] = useState(0)
+  const boxRef = useRef<HTMLDivElement>(null)
+
+  const updateVisibleMoviesCount = () => {
+    if (boxRef.current) {
+      const boxWidth = boxRef.current.offsetWidth
+      const cardWidth = 200
+      setVisibleMoviesCount(Math.floor(boxWidth / cardWidth))
+    }
+  }
+
+  useEffect(() => {
+    updateVisibleMoviesCount()
+    window.addEventListener('resize', updateVisibleMoviesCount)
+
+    return () => window.removeEventListener('resize', updateVisibleMoviesCount)
+  }, [])
 
   const handleNext = () => {
     setCurrent(prev => (prev + 1) % movies.length)
@@ -18,24 +35,35 @@ export const Carousel = ({ movies }: ICarousel) => {
     setCurrent(prev => (prev - 1 + movies.length) % movies.length)
   }
 
+  const renderMovies = () => {
+    const start = current
+    const end = start + visibleMoviesCount
+    return movies.slice(start, end).map((movie, index) => (
+      <Card key={index}>
+        <img
+          style={{ objectFit: 'cover', borderRadius: '8px', width: '100%' }}
+          src={movie.thumbnail}
+          alt={`Image ${index}`}
+        />
+      </Card>
+    ))
+  }
+
   return (
     <MuiBox
+      ref={boxRef}
       sx={{
         display: 'flex',
+        overflow: 'hidden',
         justifyContent: 'center',
         alignItems: 'center',
         width: '100%',
         border: '1px solid coral',
+        position: 'relative',
       }}
     >
       <PrevArrow onClick={handlePrev} />
-      <Card>
-        <img
-          style={{ objectFit: 'cover', borderRadius: '8px', width: '100%' }}
-          src={movies[current].thumbnail}
-          alt={`Image ${current}`}
-        />
-      </Card>
+      {renderMovies()}
       <NextArrow onClick={handleNext} />
     </MuiBox>
   )
--- src/components/carousel/style.ts
+++ src/components/carousel/style.ts
@@ -2,27 +2,31 @@ import NavigateBeforeIcon from '@mui/icons-material/NavigateBefore'
 import NavigateNextIcon from '@mui/icons-material/NavigateNext'
 import { Box as MuiBox, styled } from '@mui/material'
 
-export const Card = styled(MuiBox)(({ theme }) => ({
+export const Card = styled(MuiBox)(() => ({
   display: 'flex',
   justifyContent: 'center',
   borderRadius: '8px',
-  width: '250px',
-
-  // [theme.breakpoints.down('sm')]: {
-
-  // },
+  width: '200px',
+  margin: '0 8px',
 }))
 
 export const PrevArrow = styled(NavigateBeforeIcon)(({ theme }) => ({
   fontSize: '80px',
-  border: '1px solid white',
   color: theme.palette.secondary.main,
+  background: 'transparent',
   cursor: 'pointer',
-  textDecoration: 'none',
+  position: 'absolute',
+  left: 0,
+  top: '50%',
+  transform: 'translateY(-50%)',
 }))
 export const NextArrow = styled(NavigateNextIcon)(({ theme }) => ({
   fontSize: '80px',
-  border: '1px solid white',
+  background: 'transparent',
   color: theme.palette.secondary.main,
   cursor: 'pointer',
+  position: 'absolute',
+  right: 0,
+  top: '50%',
+  transform: 'translateY(-50%)',
 }))
