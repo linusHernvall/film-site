91006df907f1c5818cf9c440abeb78a86c62af62
Jenny Weijland <jennyweij@outlook.com>
2023-12-06 12:26:33 +0100

    removed h2 and p tags and added Typogrpahy instead now that the theme works again

--- src/MovieCard.tsx
+++ src/MovieCard.tsx
@@ -1,53 +1,30 @@
-import { Box } from '@mui/material'
+import { Box, Typography } from '@mui/material'
 
 function MovieCard() {
-  // Define your styles at the bottom of your code
-  const styles = {
-    movieCard: {
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center',
-    },
-    movieImage: {
-      height: '400px',
-      width: '300px',
-      objectFit: 'cover',
-    },
-    movieTitle: {
-      color: 'white',
-      fontFamily: 'Bebas Neue',
-    },
-    movieGenre: {
-      color: 'white',
-      fontFamily: 'Bebas Neue',
-    },
-    movieDescription: {
-      margin: '16px 0',
-    },
-  }
-
   return (
     <Box>
       <img src='/poster.jpg' alt='Movie Poster' />
 
       {/* Box with movie description */}
       <Box>
-        <h2 style={styles.movieTitle}>Oppenheimer</h2>
-        <h3 style={styles.movieGenre}>Genre: Drama</h3>
+        <Typography variant='h1'>Oppenheimer</Typography>
+        <Typography variant='h3'>Genre: Drama</Typography>
       </Box>
 
-      <Box style={styles.movieDescription}>
-        <p>
+      <Box>
+        <Typography variant='body1'>
           A mysterious stranger with a harmonica joins forces with a notorious desperado to protect
           a beautiful widow from a ruthless assassin working for the railroad.
-        </p>
+        </Typography>
       </Box>
 
       {/* Box with movie information */}
       <Box>
-        <p>Year: 2023</p>
-        <p>Rating: PG-13</p>
-        <p>Cast: Cillian Murphy, Robert Downey Jr, Emily BLunt, Matt Damon</p>
+        <Typography variant='body1'>Year: 2023</Typography>
+        <Typography variant='body1'>Rating: PG-13</Typography>
+        <Typography variant='body1'>
+          Cast: Cillian Murphy, Robert Downey Jr, Emily BLunt, Matt Damon
+        </Typography>
       </Box>
     </Box>
   )
