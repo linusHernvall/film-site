b0a4e4464df81f1e19933c0292971fad6aa85287
Jenny Weijland <jennyweij@outlook.com>
2023-12-06 12:31:47 +0100

    added margins

--- src/MovieCard.tsx
+++ src/MovieCard.tsx
@@ -2,30 +2,26 @@ import { Box, Typography } from '@mui/material'
 
 function MovieCard() {
   return (
-    <Box>
+    <Box display='flex' flexDirection='column' marginLeft='20px' marginRight='20px'>
       <img src='/poster.jpg' alt='Movie Poster' />
 
       {/* Box with movie description */}
-      <Box>
-        <Typography variant='h1'>Oppenheimer</Typography>
-        <Typography variant='h3'>Genre: Drama</Typography>
-      </Box>
 
-      <Box>
-        <Typography variant='body1'>
-          A mysterious stranger with a harmonica joins forces with a notorious desperado to protect
-          a beautiful widow from a ruthless assassin working for the railroad.
-        </Typography>
-      </Box>
+      <Typography variant='h1'>Oppenheimer</Typography>
+      <Typography variant='h3'>Genre: Drama</Typography>
+
+      <Typography variant='body1'>
+        A mysterious stranger with a harmonica joins forces with a notorious desperado to protect a
+        beautiful widow from a ruthless assassin working for the railroad.
+      </Typography>
 
       {/* Box with movie information */}
-      <Box>
-        <Typography variant='body1'>Year: 2023</Typography>
-        <Typography variant='body1'>Rating: PG-13</Typography>
-        <Typography variant='body1'>
-          Cast: Cillian Murphy, Robert Downey Jr, Emily BLunt, Matt Damon
-        </Typography>
-      </Box>
+
+      <Typography variant='body1'>Year: 2023</Typography>
+      <Typography variant='body1'>Rating: PG-13</Typography>
+      <Typography variant='body1'>
+        Cast: Cillian Murphy, Robert Downey Jr, Emily BLunt, Matt Damon
+      </Typography>
     </Box>
   )
 }
