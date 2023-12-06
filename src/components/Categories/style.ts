0badbab802b81d42660f477f33d68ba209e813db
MadeleineGustafson <madeleinegu@gmail.com>
2023-12-06 13:51:50 +0100

    proper styling with hover effect

--- src/components/Categories/style.ts
+++ src/components/Categories/style.ts
@@ -1,15 +1,13 @@
-import { Box, Typography, styled } from '@mui/material'
+import { Box, styled } from '@mui/material'
 
 export const CategoryBox = styled(Box)(({ theme }) => ({
   width: 170,
   height: 170,
-  backgroundColor: theme.palette.secondary.main,
+  backgroundColor: theme.palette.secondary[800],
   display: 'flex',
   justifyContent: 'center',
   alignItems: 'center',
   '&:hover': {
-    backgroundColor: theme.palette.secondary.main,
+    backgroundColor: theme.palette.secondary[700],
   },
 }))
-
-export const TextTitle = styled(Typography)(() => ({}))
