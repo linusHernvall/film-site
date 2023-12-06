544e1511a5bc3023eb9642d147926ef060dcf054
Nathalie Gustafsson <nath.gustafsson@gmail.com>
2023-12-06 16:52:21 +0100

    add title on Bookmark page

--- src/Pages/Bookmarked.tsx
+++ src/Pages/Bookmarked.tsx
@@ -1,5 +1,11 @@
+import { Box as MuiBox, Typography } from '@mui/material'
+
 function Bookmarked() {
-  return <div>Bookmarked page</div>
+  return (
+    <MuiBox sx={{ display: 'flex', justifyContent: 'center' }}>
+      <Typography variant='h1'>Your list</Typography>
+    </MuiBox>
+  )
 }
 
 export default Bookmarked
