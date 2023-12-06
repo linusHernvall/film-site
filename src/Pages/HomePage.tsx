10415f30ca420d9ee1a8600519f08730ee286af3
Linus Hernvall <linus.hernvall@gmail.com>
2023-12-06 10:14:55 +0100

    correct themeProvider

--- src/Pages/HomePage.tsx
+++ src/Pages/HomePage.tsx
@@ -1,5 +1,12 @@
+import movieData from '../../data/movies.json'
+import Carousel from '../components/carousel/Carousel'
+
 function HomePage() {
-  return <div>HomePage</div>
+  return (
+    <div>
+      <Carousel movies={movieData} />
+    </div>
+  )
 }
 
 export default HomePage
