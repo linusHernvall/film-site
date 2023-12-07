4d6663f30d0b9ea0c2f6906e5bdee490f1db02e0
Nathalie Gustafsson <nath.gustafsson@gmail.com>
2023-12-07 12:58:30 +0100

    add ThumbnailCard to Bookmarked page

--- src/Pages/Bookmarked.tsx
+++ src/Pages/Bookmarked.tsx
@@ -1,5 +1,13 @@
+import { Typography } from '@mui/material'
+import ThumbnailCard from '../components/thumbnailCard/thumbnailCard'
+
 function Bookmarked() {
-  return <div>Bookmarked page</div>
+  return (
+    <div>
+      <Typography variant='h1'>Bookmarked page</Typography>
+      <ThumbnailCard />
+    </div>
+  )
 }
 
 export default Bookmarked
