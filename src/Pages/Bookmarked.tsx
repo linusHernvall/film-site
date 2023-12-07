8709fa12c47fe25e5931c3f7a0039c6e8fa651ee
Nathalie Gustafsson <nath.gustafsson@gmail.com>
2023-12-07 13:50:35 +0100

    add mapping on bookmarked page

--- src/Pages/Bookmarked.tsx
+++ src/Pages/Bookmarked.tsx
@@ -1,11 +1,14 @@
 import { Typography } from '@mui/material'
+import moviesData from '../../data/movies.json'
 import ThumbnailCard from '../components/thumbnailCard/thumbnailCard'
 
 function Bookmarked() {
   return (
     <div>
       <Typography variant='h1'>Bookmarked page</Typography>
-      <ThumbnailCard />
+      {moviesData.map((movie, index) => (
+        <ThumbnailCard key={index} movie={movie} />
+      ))}
     </div>
   )
 }
