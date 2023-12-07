d2f632acbe9b18fdfea008af109d5f72fe6b0bea
Nathalie Gustafsson <nath.gustafsson@gmail.com>
2023-12-07 16:33:53 +0100

    updated import

--- src/Pages/Bookmarked.tsx
+++ src/Pages/Bookmarked.tsx
@@ -1,6 +1,6 @@
 import { Typography } from '@mui/material'
 import moviesData from '../../data/movies.json'
-import ThumbnailCard from '../components/thumbnailCard/thumbnailCard'
+import ThumbnailCard from '../components/thumbnailCard/ThumbnailCard'
 
 function Bookmarked() {
   return (
