3518527f8741f380b145be8d0c4fbb3ee257a4bd
Jenny Weijland <jennyweij@outlook.com>
2023-12-08 12:47:29 +0100

    removed out commented and unneccessary code

--- src/components/thumbnailCard/ThumbnailCard.tsx
+++ src/components/thumbnailCard/ThumbnailCard.tsx
@@ -12,14 +12,12 @@ function ThumbnailCard({ movie }: ThumbnailCardProps) {
   const { title, year, rating, thumbnail } = movie
   const [imageError, setImageError] = useState(false)
   const navigate = useNavigate()
-  // const { title: filmTitle } = useParams<{ title: string }>()
 
   const imageSource = imageError
     ? 'https://bfl-bred.com/wp-content/themes/finacia/assets/images/no-image/No-Image-Found-400x264.png'
     : thumbnail
 
   const handleClick = () => {
-    // Replace spaces with hyphens in the title parameter
     const formattedTitle = title.replace(/\s+/g, '-')
     navigate(`/filmview/${formattedTitle}`)
   }
