1fe18d6559bc069d8b4951c6cc0644dbb84bbc92
MadeleineGustafson <madeleinegu@gmail.com>
2023-12-08 13:55:17 +0100

    Footer created and styled

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Box } from '@mui/material'
 import { Outlet } from 'react-router'
 import Header from './components/Header/Header'
+import Footer from './components/footer/Footer'
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
     >
       <Header />
       <Outlet />
+      <Footer />
     </Box>
   )
 }
---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,72 @@
+import FacebookIcon from '@mui/icons-material/Facebook'
+import InstagramIcon from '@mui/icons-material/Instagram'
+import TwitterIcon from '@mui/icons-material/Twitter'
+import { Box } from '@mui/material'
+import { theme } from '../../theme'
+import { FooterBox, FooterContainer, Footertext } from './style'
+
+function Footer() {
+  return (
+    <FooterContainer>
+      <FooterBox>
+        <Box
+          sx={{
+            display: 'flex',
+            justifyContent: 'space-between',
+            [theme.breakpoints.down('sm')]: {
+              flexDirection: 'column',
+              gap: '1rem',
+            },
+          }}
+        >
+          <Box
+            style={{
+              display: 'flex',
+              flexDirection: 'column',
+            }}
+          >
+            <Footertext>Mediecenter</Footertext>
+            <Footertext>Sekretess</Footertext>
+            <Footertext>Kontakta oss</Footertext>
+          </Box>
+          <Box
+            style={{
+              display: 'flex',
+              flexDirection: 'column',
+            }}
+          >
+            <Footertext>Presentkort</Footertext>
+            <Footertext>Användarvillkor</Footertext>
+            <Footertext>Företagsinformation</Footertext>
+          </Box>
+          <Box
+            style={{
+              display: 'flex',
+              flexDirection: 'column',
+            }}
+          >
+            <Footertext>Hjälpcenter</Footertext>
+            <Footertext>Jobba hos oss</Footertext>
+            <Footertext>Cookie-inställningar</Footertext>
+          </Box>
+        </Box>
+      </FooterBox>
+      <Box
+        style={{
+          //   justifyContent: 'center',
+          width: '100%',
+          display: 'flex',
+          color: 'white',
+          gap: '2rem',
+          paddingTop: '1.5rem',
+        }}
+      >
+        <FacebookIcon />
+        <InstagramIcon />
+        <TwitterIcon />
+      </Box>
+    </FooterContainer>
+  )
+}
+
+export default Footer
