c6bdf375e5bfbf1d95b12e58f195644a48d08fc0
Linus Hernvall <linus.hernvall@gmail.com>
2023-12-08 14:54:21 +0100

    feat: displaying error messages

--- src/Pages/HomePage.tsx
+++ src/Pages/HomePage.tsx
@@ -4,12 +4,27 @@ import movieData from '../../data/movies.json'
 import MovieCard from '../components/MovieCard/MovieCard'
 import SearchBar from '../components/searchBar/SearchBar'
 import { Movie } from '../interface/interfaces'
+import { theme } from '../theme'
 
 function HomePage() {
   const [searchWord, setSearchWord] = useState('')
   const [searchResults, setSearchResults] = useState<Movie[]>([])
   const [isError, setIsError] = useState(false)
   const [noResults, setNoResults] = useState(false)
+
+  // const handleSearch = () => {
+  //   if (searchWord.trim() === '') {
+  //     setIsError(true)
+  //     setNoResults(false)
+  //   } else {
+  //     setIsError(false)
+  //     const filteredMovies = movieData.filter(movie =>
+  //       movie.title.toLowerCase().includes(searchWord.toLowerCase())
+  //     )
+  //     setSearchResults(filteredMovies)
+  //     setNoResults(filteredMovies.length === 0)
+  //   }
+  // }
 
   const handleSearch = () => {
     if (searchWord.trim() === '') {
@@ -20,8 +35,22 @@ function HomePage() {
       const filteredMovies = movieData.filter(movie =>
         movie.title.toLowerCase().includes(searchWord.toLowerCase())
       )
-      setSearchResults(filteredMovies)
-      setNoResults(filteredMovies.length === 0)
+      if (filteredMovies.length > 0) {
+        setSearchResults(filteredMovies)
+        setNoResults(false)
+      } else {
+        setNoResults(true)
+      }
+    }
+  }
+
+  const getMessage = () => {
+    if (isError) {
+      return 'Please enter a search term.'
+    } else if (noResults && !isError) {
+      return 'No movies found matching your search.'
+    } else {
+      return ''
     }
   }
 
@@ -29,21 +58,21 @@ function HomePage() {
     <div>
       <Typography variant='h1'>Home Page</Typography>
       <SearchBar searchWord={searchWord} setSearchWord={setSearchWord} onSubmit={handleSearch} />
-      {isError && (
-        <Box>
-          <Typography>Please enter a search term.</Typography>
-        </Box>
-      )}
 
-      {noResults && (
-        <Box>
-          <Typography>No movies found matching your search.</Typography>
-        </Box>
-      )}
+      {/* Single Box for displaying messages or acting as a filler */}
+      <Box sx={{ minHeight: '24px' }}>
+        {' '}
+        <Typography
+          variant='body1'
+          color={isError || noResults ? theme.palette.secondary.main : 'transparent'}
+        >
+          {getMessage()}
+        </Typography>
+      </Box>
 
-      {!isError &&
-        !noResults &&
-        searchResults.map(movie => <MovieCard key={movie.title} movie={movie} />)}
+      {searchResults.map(movie => (
+        <MovieCard key={movie.title} movie={movie} />
+      ))}
     </div>
   )
 }
--- src/components/MovieCard/style.ts
+++ src/components/MovieCard/style.ts
@@ -1,29 +1,31 @@
-import { Box, Grid, styled } from '@mui/material';
+import { Box, Grid, styled } from '@mui/material'
+import { theme } from '../../theme'
 
 export const MovieCardContainer = styled(Grid)(() => ({
   display: 'flex',
   justifyContent: 'center',
-  maxWidth: "800px",
-  alignItems: "center",
-  marginTop: "20px"
-}));
+  maxWidth: '800px',
+  alignItems: 'center',
+  marginTop: '20px',
+}))
 
 export const MovieInfoBox = styled(Box)(() => ({
   display: 'flex',
   flexDirection: 'column',
   marginLeft: '40px',
   marginRight: '40px',
-}));
+}))
 
 export const MovieInfoItemBox = styled(Box)(() => ({
   display: 'flex',
   alignItems: 'center',
-}));
+}))
 
 export const MoviePosterImage = styled('img')({
   width: '90%',
   height: 'auto',
-});
+  borderRadius: theme.shape.borderRadius,
+})
 
 export const VerticalLine = styled(Box)(({ theme }) => ({
   height: '70px',
@@ -31,4 +33,4 @@ export const VerticalLine = styled(Box)(({ theme }) => ({
   backgroundColor: theme.palette.primary[800],
   marginRight: '10px',
   marginLeft: '-10px',
-}));
+}))
