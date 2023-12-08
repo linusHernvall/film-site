d7f1f5fb3e36c6d0461e7464da18f34c78b4609f
Nathalie Gustafsson <nath.gustafsson@gmail.com>
2023-12-08 09:56:53 +0100

    styling on mapped out thumnailCards (are not filtered yet)

--- src/Pages/Bookmarked.tsx
+++ src/Pages/Bookmarked.tsx
@@ -1,14 +1,33 @@
-import { Typography } from '@mui/material'
+import { Box, Typography } from '@mui/material'
 import moviesData from '../../data/movies.json'
 import ThumbnailCard from '../components/thumbnailCard/ThumbnailCard'
 
 function Bookmarked() {
   return (
-    <div>
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
       <Typography variant='h1'>Bookmarked page</Typography>
-      {moviesData.map((movie, index) => (
-        <ThumbnailCard key={index} movie={movie} />
-      ))}
+      <Box
+        sx={{
+          display: 'flex',
+          flexWrap: 'wrap',
+          gap: '2rem',
+          justifyContent: 'center',
+          alignItems: 'center',
+          marginTop: '3rem',
+          padding: '2rem'
+        }}
+      >
+        {moviesData.map((movie, index) => (
+          <ThumbnailCard key={index} movie={movie} />
+        ))}
+      </Box>
     </div>
   )
 }
