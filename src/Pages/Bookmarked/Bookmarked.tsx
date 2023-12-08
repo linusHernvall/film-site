4523f91c555e3e0e89782b0b8e436fbb001b096b
Nathalie Gustafsson <nath.gustafsson@gmail.com>
2023-12-08 10:15:16 +0100

    moved style to a style file instead

---
+++ src/Pages/Bookmarked/Bookmarked.tsx
@@ -0,0 +1,19 @@
+import { Typography } from '@mui/material'
+import moviesData from '../../../data/movies.json'
+import ThumbnailCard from '../../components/thumbnailCard/ThumbnailCard'
+import { ContainerBox, ThumbnailBox } from './style'
+
+function Bookmarked() {
+  return (
+    <ContainerBox>
+      <Typography variant='h1'>Bookmarked page</Typography>
+      <ThumbnailBox>
+        {moviesData.map((movie, index) => (
+          <ThumbnailCard key={index} movie={movie} />
+        ))}
+      </ThumbnailBox>
+    </ContainerBox>
+  )
+}
+
+export default Bookmarked
