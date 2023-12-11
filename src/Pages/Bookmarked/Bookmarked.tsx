b4f3b2e62cfd948dcdd8ac229d5ac045967b3179
Nathalie Gustafsson <nath.gustafsson@gmail.com>
2023-12-11 15:16:23 +0100

    add text if there's no bookmarked title

--- src/Pages/Bookmarked/Bookmarked.tsx
+++ src/Pages/Bookmarked/Bookmarked.tsx
@@ -8,13 +8,17 @@ function Bookmarked() {
 
   return (
     <ContainerBox>
-      <Typography variant='h1'>Bookmarked movies</Typography>
+    <Typography variant='h1'>Your List</Typography>
+    {bookmarkedMovies.length > 0 ? (
       <ThumbnailBox>
         {bookmarkedMovies.map((movie, index) => (
           <ThumbnailCard key={index} movie={movie} />
         ))}
       </ThumbnailBox>
-    </ContainerBox>
+    ) : (
+      <Typography variant='body1'>You haven’t added any titles to your list yet.</Typography>
+    )}
+  </ContainerBox>
   )
 }
 
