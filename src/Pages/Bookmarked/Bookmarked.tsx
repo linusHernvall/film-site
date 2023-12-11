fb6978ef84da21b5540a406b8ad58bf10cae4013
Nathalie Gustafsson <nath.gustafsson@gmail.com>
2023-12-11 15:28:00 +0100

    add text if there's no added bookmark

--- src/Pages/Bookmarked/Bookmarked.tsx
+++ src/Pages/Bookmarked/Bookmarked.tsx
@@ -1,24 +1,26 @@
 import { Typography } from '@mui/material'
 import ThumbnailCard from '../../components/thumbnailCard/ThumbnailCard'
-import { ContainerBox, ThumbnailBox } from './style'
 import { useThumbnailContext } from '../../context/ThumbnailContext'
+import { ContainerBox, NoBookmarkedBox, ThumbnailBox } from './style'
 
 function Bookmarked() {
   const { bookmarkedMovies } = useThumbnailContext()
 
   return (
     <ContainerBox>
-    <Typography variant='h1'>Your List</Typography>
-    {bookmarkedMovies.length > 0 ? (
-      <ThumbnailBox>
-        {bookmarkedMovies.map((movie, index) => (
-          <ThumbnailCard key={index} movie={movie} />
-        ))}
-      </ThumbnailBox>
-    ) : (
-      <Typography variant='body1'>You haven’t added any titles to your list yet.</Typography>
-    )}
-  </ContainerBox>
+      <Typography variant='h1'>Your List</Typography>
+      {bookmarkedMovies.length > 0 ? (
+        <ThumbnailBox>
+          {bookmarkedMovies.map((movie, index) => (
+            <ThumbnailCard key={index} movie={movie} />
+          ))}
+        </ThumbnailBox>
+      ) : (
+        <NoBookmarkedBox>
+          <Typography variant='body1'>You haven’t added any titles to your list yet.</Typography>
+        </NoBookmarkedBox>
+      )}
+    </ContainerBox>
   )
 }
 
