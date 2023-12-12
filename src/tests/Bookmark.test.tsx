d996fb9cb43d7bfbd90acff352fe58d41441cc4f
Nathalie Gustafsson <nath.gustafsson@gmail.com>
2023-12-12 20:47:12 +0100

    tried another approach with the tests, unsure if they're correct though

--- src/tests/Bookmark.test.tsx
+++ src/tests/Bookmark.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen, waitFor } from '@testing-library/react'
+import { render, screen, waitFor, within } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import { MemoryRouter, Route, Routes } from 'react-router'
 import { expect, test } from 'vitest'
@@ -7,7 +7,7 @@ import CategorySpecific from '../Pages/CategorySpecific'
 import Header from '../components/Header/Header'
 import { ThumbnailProvider } from '../context/ThumbnailContext'
 
-const mockedMovie = {
+const mockedMovie1 = {
   title: 'Casablanca',
   year: 1942,
   rating: 'PG',
@@ -17,6 +17,18 @@ const mockedMovie = {
     'A cynical expatriate American cafe owner struggles to decide whether or not to help his former lover and her fugitive husband escape the Nazis in French Morocco.',
   thumbnail:
     'https://m.media-amazon.com/images/M/MV5BY2IzZGY2YmEtYzljNS00NTM5LTgwMzUtMzM1NjQ4NGI0OTk0XkEyXkFqcGdeQXVyNDYyMDk5MTU@._V1_QL75_UX380_CR0,5,380,562_.jpg',
+}
+
+const mockedMovie2 = {
+  title: 'Psycho',
+  year: 1960,
+  rating: 'R',
+  actors: ['Anthony Perkins', 'Janet Leigh', 'Vera Miles'],
+  genre: 'Horror, Mystery, Thriller',
+  synopsis:
+    "A Phoenix secretary embezzles $40,000 from her employer's client, goes on the run and checks into a remote motel run by a young man under the domination of his mother.",
+  thumbnail:
+    'https://m.media-amazon.com/images/M/MV5BNTQwNDM1YzItNDAxZC00NWY2LTk0M2UtNDIwNWI5OGUyNWUxXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_QL75_UX380_CR0,1,380,562_.jpg',
 }
 
 test('show a text if there is no bookmarked thumbnails on BookmarkPage', async () => {
@@ -29,50 +41,6 @@ test('show a text if there is no bookmarked thumbnails on BookmarkPage', async (
   )
 
   expect(screen.getByText('You haven’t added any titles to your list yet.')).toBeInTheDocument()
-})
-
-test('should be possible to remove bookmarked thumbnail on bookmarked page and display text', async () => {
-  render(
-    <MemoryRouter initialEntries={['/categories/War']}>
-      <ThumbnailProvider>
-        <Header />
-        <Routes>
-          <Route path='/categories/:genre' element={<CategorySpecific />} />
-          <Route path='/bookmarked' element={<Bookmarked />} />
-        </Routes>
-      </ThumbnailProvider>
-    </MemoryRouter>
-  )
-  const user = userEvent.setup()
-
-  expect(screen.getByText('CATEGORIES/War')).toBeInTheDocument()
-
-  expect(screen.getByText(mockedMovie.title)).toBeInTheDocument()
-
-  user.click(screen.getByText('favorite'))
-
-  const bookmarkPageButton = screen.getByTestId('FavoriteRoundedIcon')
-  expect(bookmarkPageButton).toBeInTheDocument()
-
-  user.click(bookmarkPageButton)
-
-  const bookmarkPageTitle = await screen.findByText('Your List')
-  expect(bookmarkPageTitle).toBeInTheDocument()
-
-  const bookmarkedMovieTitle = await screen.findByText(mockedMovie.title)
-  expect(bookmarkedMovieTitle).toBeInTheDocument()
-
-  await user.click(screen.getByText('favorite'))
-
-  await waitFor(() => {
-    const bookmarkedMovieYearRemoved = screen.queryByText(mockedMovie.title)
-    expect(bookmarkedMovieYearRemoved).not.toBeInTheDocument()
-  })
-
-  const emptyBookmarkPageText = await screen.findByText(
-    'You haven’t added any titles to your list yet.'
-  )
-  expect(emptyBookmarkPageText).toBeInTheDocument()
 })
 
 test('should be possible to add bookmarked thumbnail to bookmarked page', async () => {
@@ -91,7 +59,7 @@ test('should be possible to add bookmarked thumbnail to bookmarked page', async 
 
   expect(screen.getByText('CATEGORIES/War')).toBeInTheDocument()
 
-  expect(screen.getByText(mockedMovie.title)).toBeInTheDocument()
+  expect(screen.getByText(mockedMovie1.title)).toBeInTheDocument()
 
   user.click(screen.getByText('favorite'))
 
@@ -103,6 +71,47 @@ test('should be possible to add bookmarked thumbnail to bookmarked page', async 
   const bookmarkPageTitle = await screen.findByText('Your List')
   expect(bookmarkPageTitle).toBeInTheDocument()
 
-  const bookmarkedMovieTitle = await screen.findByText(mockedMovie.title)
+  const bookmarkedMovieTitle = await screen.findByText(mockedMovie1.title)
   expect(bookmarkedMovieTitle).toBeInTheDocument()
+})
+
+test('should be possible to remove bookmarked thumbnail on bookmarked page and display text', async () => {
+  render(
+    <MemoryRouter initialEntries={['/categories/Horror']}>
+      <ThumbnailProvider>
+        <Header />
+        <Routes>
+          <Route path='/categories/:genre' element={<CategorySpecific />} />
+          <Route path='/bookmarked' element={<Bookmarked />} />
+        </Routes>
+      </ThumbnailProvider>
+    </MemoryRouter>
+  )
+  const user = userEvent.setup()
+
+  expect(screen.getByText('CATEGORIES/Horror')).toBeInTheDocument()
+
+  expect(screen.getByText(mockedMovie2.title)).toBeInTheDocument()
+
+  user.click(screen.getByText('favorite'))
+
+  const bookmarkPageButton = screen.getByTestId('FavoriteRoundedIcon')
+  expect(bookmarkPageButton).toBeInTheDocument()
+
+  user.click(bookmarkPageButton)
+
+  const bookmarkPageTitle = await screen.findByText('Your List')
+  expect(bookmarkPageTitle).toBeInTheDocument()
+
+  const psychoCard = screen.getByText(mockedMovie2.title).parentElement
+  expect(psychoCard).toBeInTheDocument()
+
+  const heartBtn = within(psychoCard!).getByText('favorite')
+
+  await user.click(heartBtn)
+
+  await waitFor(() => {
+    const bookmarkedMovieYearRemoved = screen.queryByText(mockedMovie2.title)
+    expect(bookmarkedMovieYearRemoved).not.toBeInTheDocument()
+  })
 })
