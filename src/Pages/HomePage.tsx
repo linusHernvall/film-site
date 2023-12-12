6d6f6f4f111e4f69a2c29ccb1c10d507c4845a0e
Jenny Weijland <jennyweij@outlook.com>
2023-12-12 09:44:42 +0100

    removed the moviecards from the homepage

--- src/Pages/HomePage.tsx
+++ src/Pages/HomePage.tsx
@@ -1,12 +1,11 @@
 import { Box, Typography } from '@mui/material'
 import { useState } from 'react'
-import movieData from '../../data/movies.json'
+import { default as movieData } from '../../data/movies.json'
 import MovieCarousel from '../components/moviecarousel/MovieCarousel'
 import SearchBar from '../components/searchBar/SearchBar'
 import ThumbnailCard from '../components/thumbnailCard/ThumbnailCard'
 import { Movie } from '../interface/interfaces'
 import { theme } from '../theme'
-import moviesData from '../../data/movies.json'
 
 function HomePage() {
   const [searchMovie, setSearchMovie] = useState('')
@@ -68,12 +67,6 @@ function HomePage() {
         ))}
       </Box>
       <MovieCarousel />
-
-      <div>
-        {moviesData.map((movie, index) => (
-          <ThumbnailCard key={index} movie={movie} />
-        ))}
-      </div>
     </div>
   )
 }
