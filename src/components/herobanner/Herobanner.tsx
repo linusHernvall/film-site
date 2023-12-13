e47a12b7deb5aec9ab600406a6f0e82ef1f5b24e
Nathalie Gustafsson <nath.gustafsson@gmail.com>
2023-12-13 13:03:58 +0100

    add styling for responsivity

--- src/components/herobanner/Herobanner.tsx
+++ src/components/herobanner/Herobanner.tsx
@@ -1,12 +1,16 @@
 import { Box, CardMedia } from '@mui/material'
 import {
+  BtnText,
   ButtonContainer,
   InfoButton,
   InfoContainer,
+  InfoText,
+  InfoTextContainer,
   PlayButton,
   Title,
   TitleAndButtonContainer,
 } from './style'
+import { theme } from '../../theme'
 
 function Herobanner() {
   return (
@@ -16,30 +20,45 @@ function Herobanner() {
         autoPlay
         loop
         muted
-        sx={{ width: '100%', height: '600px', objectFit: 'cover' }}
+        sx={{
+          width: '100%',
+          height: '600px',
+          objectFit: 'cover',
+          [theme.breakpoints.down('sm')]: {
+            height: '500px',
+          },
+        }}
         src='https://www.pexels.com/sv-se/download/video/2711111/?fps=24.0&h=720&w=1280'
       ></CardMedia>
       <InfoContainer>
         <TitleAndButtonContainer>
           <Title variant='h1'>Nature's Adventure</Title>
           <ButtonContainer>
-            <PlayButton style={{ display: 'flex,', alignItems: 'center' }}>
-              <span className='material-symbols-outlined'>play_arrow</span>
-              Play
+            <PlayButton>
+              <span
+                style={{
+                  fontVariationSettings: `'FILL' 1`,
+                }}
+                className='material-symbols-outlined'
+              >
+                play_arrow
+              </span>
+              <BtnText variant='body2'>Play</BtnText>
             </PlayButton>
-            <InfoButton style={{ display: 'flex,', alignItems: 'center' }}>
-              <span className='material-symbols-outlined'>info</span>More info
+            <InfoButton>
+              <span className='material-symbols-outlined'>info</span>
+              <BtnText variant='body2'>More info</BtnText>
             </InfoButton>
           </ButtonContainer>
         </TitleAndButtonContainer>
-        <div style={{ width: '500px' }}>
-          <p style={{ color: 'white' }}>
+        <InfoTextContainer>
+          <InfoText variant='body1'>
             Emily, an adventurous biologist, sets out to uncover the secrets of an untouched
             wilderness. As she immerses herself in the diverse landscapes, from towering mountains
             to serene lakeshores, the film becomes a celebration of nature's resilience and the
             intrinsic connection between humanity and the great outdoors.
-          </p>
-        </div>
+          </InfoText>
+        </InfoTextContainer>
       </InfoContainer>
     </Box>
   )
--- src/components/herobanner/style.ts
+++ src/components/herobanner/style.ts
@@ -1,15 +1,27 @@
 import { Box, Button, Typography, styled } from '@mui/material'
 
-export const InfoContainer = styled(Box)(() => ({
+export const InfoContainer = styled(Box)(({theme}) => ({
   position: 'absolute',
   left: '100px',
   top: '350px',
+  width: '500px',
+  [theme.breakpoints.down('sm')]: {
+    width: '300px',
+    left: '20px',
+    top: '300px'
+  },
 }))
 
-export const TitleAndButtonContainer = styled(Box)(() => ({
+export const TitleAndButtonContainer = styled(Box)(({theme}) => ({
   display: 'flex',
   alignItems: 'center',
   gap: '2rem',
+  [theme.breakpoints.down('sm')]: {
+    flexDirection: 'column',
+    alignItems: 'flex-start',
+    gap: '0rem',
+    marginBottom: '0.7rem'
+  },
 }))
 
 export const ButtonContainer = styled(Box)(() => ({
@@ -23,10 +35,37 @@ export const Title = styled(Typography)(() => ({}))
 
 export const PlayButton = styled(Button)(() => ({
   backgroundColor: 'white',
-  color: 'black'
+  color: 'black',
+  width: '100px',
+  display: 'flex',
+  alignItems: 'center',
+  height: '100%',
+  borderRadius: '5px',
+  gap: '0.1rem',
 }))
 
 export const InfoButton = styled(Button)(() => ({
   backgroundColor: 'lightgrey',
-  color: 'black'
+  color: 'black',
+  width: '100px',
+  display: 'flex',
+  alignItems: 'center',
+  height: '100%',
+  gap: '0.2rem',
+  borderRadius: '5px',
+}))
+
+export const InfoTextContainer = styled(Box)(() => ({
+  width: '100%',
+}))
+
+export const InfoText = styled(Typography)(({theme}) => ({
+  color: 'white',
+  [theme.breakpoints.down('sm')]: {
+   fontSize: '12px'
+  },
+}))
+
+export const BtnText = styled(Typography)(() => ({
+  color: 'black',
 }))
